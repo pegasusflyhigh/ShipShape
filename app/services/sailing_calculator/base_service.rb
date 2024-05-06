fbf667bac4353086314c290574c21a1908a78f87
Bhuvna Prabhu <bbhuvnaprabhu@gmail.com>
2024-05-07 00:09:05 +0200

    Refactor cheapest direct sailing service

--- app/services/sailing_calculator/base_service.rb
+++ app/services/sailing_calculator/base_service.rb
@@ -10,20 +10,16 @@ module SailingCalculator
       @destination_port_code = destination_port_code
       @exchange_rate_cache = {}
       @min_sailing_rate = nil
-      @min_direct_sailing_option = nil
+      @min_sailing_option = nil
     end
 
     private
 
-    attr_reader :origin_port_code, :destination_port_code, :min_sailing_rate, :min_direct_sailing_option
+    attr_reader :origin_port_code, :destination_port_code, :min_sailing_rate, :min_sailing_option
 
-    def origin_port
-      @origin_port ||= Port.find_by(code: origin_port_code)
-    end
-
-    def destination_port
-      @destination_port ||= Port.find_by(code: destination_port_code)
-    end
+    def no_sailing_options_present?
+      [origin_port, destination_port, sailing_options].any?(&:blank?)
+		end
 
     def cached_exchange_rate(date, currency_code)
       @exchange_rate_cache[date] ||= {}
@@ -40,12 +36,24 @@ module SailingCalculator
       {
         origin_port: origin_port_code,
         destination_port: destination_port_code,
-        departure_date: min_direct_sailing_option.departure_date.to_s,
-        arrival_date: min_direct_sailing_option.arrival_date.to_s,
+        departure_date: min_sailing_option.departure_date.to_s,
+        arrival_date: min_sailing_option.arrival_date.to_s,
         sailing_code: min_sailing_rate.sailing_code,
         rate: min_sailing_rate.rate,
         rate_currency: min_sailing_rate.currency_code
       }
+    end
+
+		def origin_port
+      @origin_port ||= Port.find_by(code: origin_port_code)
+    end
+
+    def destination_port
+      @destination_port ||= Port.find_by(code: destination_port_code)
+    end
+
+    def sailing_options
+      nil
     end
 
   end
--- app/services/sailing_calculator/cheapest_direct_sailing_service.rb
+++ app/services/sailing_calculator/cheapest_direct_sailing_service.rb
@@ -6,52 +6,48 @@ module SailingCalculator
     def call
       return ServiceResponse.error(I18n.t(:no_direct_sailing_found)) if no_sailing_options_present?
 
-      calculate_cheaptest_option
+      calculate_cheapest_option
 
       ServiceResponse.success(result)
     end
 
     private
 
-    def no_sailing_options_present?
-      [origin_port, destination_port, direct_sailing_options].any?(&:blank?)
-    end
-
-    def calculate_cheaptest_option
+    def calculate_cheapest_option
       min_rate = INFINITY
 
-      direct_sailing_options.find_each do |direct_sailing_option|
-        min_sailing_rate = find_min_sailing_rate(direct_sailing_option)
+      sailing_options.find_each do |sailing_option|
+        min_sailing_rate = find_min_sailing_rate(sailing_option)
 
         next unless min_sailing_rate
 
-        rate_value = calculate_rate(direct_sailing_option, min_sailing_rate)
+        rate_value = calculate_rate(sailing_option, min_sailing_rate)
         next unless rate_value < min_rate
 
-        @min_direct_sailing_option = direct_sailing_option
+        @min_sailing_option = sailing_option
         @min_sailing_rate = min_sailing_rate
         min_rate = rate_value
       end
     end
 
-    def find_min_sailing_rate(direct_sailing_option)
-      direct_sailing_option.sailing_rates.min_by do |sailing_rate|
-        calculate_rate(direct_sailing_option, sailing_rate)
+    def find_min_sailing_rate(sailing_option)
+      sailing_option.sailing_rates.min_by do |sailing_rate|
+        calculate_rate(sailing_option, sailing_rate)
       end
     end
 
-    def calculate_rate(direct_sailing_option, sailing_rate)
+    def calculate_rate(sailing_option, sailing_rate)
       return sailing_rate.rate if sailing_rate.currency_code == Currency::BASE_CURRENCY
 
-      exchange_rate = cached_exchange_rate(direct_sailing_option.departure_date, sailing_rate.currency_code)
+      exchange_rate = cached_exchange_rate(sailing_option.departure_date, sailing_rate.currency_code)
 
       return INFINITY if exchange_rate.blank?
 
       sailing_rate.rate / exchange_rate
     end
 
-    def direct_sailing_options
-      @direct_sailing_options ||= SailingOption.includes(sailing_rates: :currency)
+    def sailing_options
+      @sailing_options ||= SailingOption.includes(sailing_rates: :currency)
         .direct_between(origin_port, destination_port)
     end
 
